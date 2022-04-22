06511969a1f335d7468d1d69e9389f21289af995
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-22 17:44:05 +0200

    aggiunto 'genre' ai vari disc in data.php

--- server/data.php
+++ server/data.php
@@ -5,6 +5,7 @@ $discs = [
         'post' => 'https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg',
         'title' => 'New Jersey',
         'author' => 'Bon Jovi',
+        'genre' => 'Rock',
         'date' => '1988'
     ],
 
@@ -12,6 +13,7 @@ $discs = [
         'post' => 'https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg',
         'title' => 'Live at Wembley 86',
         'author' => 'Queen',
+        'genre' => 'Pop',
         'date' => '1992'
     ],
 
@@ -19,6 +21,7 @@ $discs = [
         'post' => 'https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg',
         'title' => "Ten's Summoner's Tales",
         'author' => 'Sting',
+        'genre' => 'Pop',
         'date' => '1993'
     ],
 
@@ -26,6 +29,7 @@ $discs = [
         'post' => 'https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg',
         'title' => 'Steve Gadd Band',
         'author' => 'Steve Gadd Band',
+        'genre' => 'Jazz',
         'date' => '2018'
     ],
 
@@ -33,6 +37,7 @@ $discs = [
         'post' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg',
         'title' => 'Brave new World',
         'author' => 'Iron Maiden',
+        'genre' => 'Metal',
         'date' => '2000'
     ],
 
@@ -40,6 +45,7 @@ $discs = [
         'post' => 'https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg',
         'title' => 'One more car, one more raider',
         'author' => 'Eric Clapton',
+        'genre' => 'Rock',
         'date' => '2002'
     ],
 
@@ -47,6 +53,7 @@ $discs = [
         'post' => 'https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg',
         'title' => 'Made in Japan',
         'author' => 'Deep Purple',
+        'genre' => 'Rock',
         'date' => '1972'
     ],
 
@@ -54,6 +61,7 @@ $discs = [
         'post' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
         'title' => 'And Justice for All',
         'author' => 'Metallica',
+        'genre' => 'Metal',
         'date' => '1988'
     ],
 
@@ -61,6 +69,7 @@ $discs = [
         'post' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg',
         'title' => 'Hard Wired',
         'author' => 'Dave Weckl',
+        'genre' => 'Jazz',
         'date' => '1994'
     ],
 
@@ -68,6 +77,7 @@ $discs = [
         'post' => 'https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg',
         'title' => 'Bad',
         'author' => 'Michael Jacjson',
+        'genre' => 'Pop',
         'date' => '1987'
     ],
 ];
